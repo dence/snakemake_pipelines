7ed45d0128216c9dc59d43f88b877a6f48e8adec
dence <dandence@gmail.com>
2020-08-27 21:22:07 -0400

    fixed cluster json in slurm script

--- pipelines_for_hipergator/fr1_project_snakefiles/run_snakemake.slurm.sh
+++ pipelines_for_hipergator/fr1_project_snakefiles/run_snakemake.slurm.sh
@@ -21,4 +21,4 @@
 unset TMPDIR
 module load python3 
 mkdir results
-snakemake --configfile config/config.yaml --snakefile ./workflow/Snakefile -c 50 --jobs 50 --directory . --cluster-config ../test.cluster.json --cluster "sbatch --qos={cluster.qos} -p {cluster.partition} -c {cluster.c} -n {cluster.N} --mail-type=FAIL --mail-user=d.ence@ufl.edu -t {cluster.time} --mem={cluster.mem} -J "fr1_align" -o fr1_align_%j.out -D /home/d.ence/projects/pinus_taeda_L/Fr1_project/test_pipelines/snakemake_pipelines/pipelines_for_hipergator/fr1_project_snakefiles"
+snakemake --configfile config/config.yaml --snakefile ./workflow/Snakefile -c 50 --jobs 50 --directory . --cluster-config ../hipergator.cluster.json --cluster "sbatch --qos={cluster.qos} -p {cluster.partition} -c {cluster.c} -n {cluster.N} --mail-type=FAIL --mail-user=d.ence@ufl.edu -t {cluster.time} --mem={cluster.mem} -J "fr1_align" -o fr1_align_%j.out -D /home/d.ence/projects/pinus_taeda_L/Fr1_project/test_pipelines/snakemake_pipelines/pipelines_for_hipergator/fr1_project_snakefiles"
